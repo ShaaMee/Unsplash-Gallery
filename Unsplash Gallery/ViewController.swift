668a16546c5f5ca6dcacc84c12cfcc7c6bc51bd6
Alexey Pavlov <laraziel@yandex.ru>
2021-11-21 11:15:12 +0300

    Initial Commit

---
+++ Unsplash Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash Gallery
+//
+//  Created by user on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
